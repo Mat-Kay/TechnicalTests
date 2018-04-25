59628e18b11d1eabd4993e47b29f1fade305ddac
Matt Kay <matthew.w.kay@gmail.com>
2018-04-26 00:39:31 +0100

    Create project structure

---
+++ CompanyA/src/WebApi/Startup.cs
@@ -0,0 +1,57 @@
+﻿namespace TechnicalTests.CompanyA.WebApi
+{
+    using System;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using StartupHelpers;
+
+    using Swashbuckle.AspNetCore.Swagger;
+
+    public class Startup
+    {
+        private const string SwaggerTitle = "Technical Test - Company A";
+        private const string SwaggerVersion = "v1";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(SwaggerVersion, new Info { Title = SwaggerTitle, Version = SwaggerVersion });
+            });
+
+            return new WindsorServiceProviderBuilder().Build(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint($"/swagger/{SwaggerVersion}/swagger.json", SwaggerTitle);
+                });
+            }
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ CompanyA/src/WebApi/StartupHelpers/WindsorServiceProviderBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace TechnicalTests.CompanyA.WebApi.StartupHelpers
+{
+    using System;
+
+    using Castle.Windsor;
+    using Castle.Windsor.MsDependencyInjection;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class WindsorServiceProviderBuilder
+    {
+        public IServiceProvider Build(IServiceCollection services)
+        {
+            var container = new WindsorContainer();
+
+            RegisterComponents(container);
+
+            return WindsorRegistrationHelper.CreateServiceProvider(container, services);
+        }
+
+        private void RegisterComponents(WindsorContainer container)
+        {
+        }
+    }
+}
